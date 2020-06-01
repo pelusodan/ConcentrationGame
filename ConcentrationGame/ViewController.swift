e3b2474ecafcc0a463c7337868bdbe763286b29d
Daniel Peluso <pelusodan@gmail.com>
2020-05-31 21:37:34 -0400

    First commit - push buttons with pretty pictures and live counter

--- ConcentrationGame/ViewController.swift
+++ ConcentrationGame/ViewController.swift
@@ -10,11 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    // FlipCount stuff
+    @IBOutlet weak var flipCountLabel: UILabel!
+    var flipCount = 0 {
+        didSet {
+            flipCountLabel.text = "Flip Count: \(flipCount)"
+        }
     }
-
-
+    var emojiChoices = ["🎧","🍖","🍏","🍓","🍋","🍎","🥑","🍒"]
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    
+    // Button stuff
+    @IBAction func touchCard(_ sender: UIButton) {
+        if let cardNumber = cardButtons.firstIndex(of: sender){
+            flipCard(withEmoji: emojiChoices[cardNumber], on: sender)
+        }else {
+            print("Error in card numbering")
+        }
+    }
+    func flipCard(withEmoji emoji: String, on button: UIButton){
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.8431372549, blue: 0.2941176471, alpha: 1)
+        } else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.1960784314, green: 0.8431372549, blue: 0.2941176471, alpha: 1)
+        }
+        flipCount += 1
+        flipCountLabel.text = "Flip Count: \(flipCount)"
+    }
+    
 }
 
